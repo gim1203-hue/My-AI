70c959920a558ab14ecf2e3e1994a129f4acfbee
gim1203-hue <gimrankhan1203@gmail.com>
2026-08-26 00:20:37 -0400

    Prepare server for hosting

--- server.js
+++ server.js
@@ -301,6 +301,8 @@ app.use(
     })
 );
 
-app.listen(3000, "127.0.0.1", () => {
-    console.log("My AI server is running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`My AI server is running on port ${PORT}`);
 });
